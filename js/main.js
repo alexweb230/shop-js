4b454aae8e5dc41c3e68f04854e17aabdaa95bd3
Александр <alex230pust@gmail.com>
2020-01-19 21:32:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const search = document.querySelector('.search');
+    const cartBtn = document.getElementById('cart');
+    const wishlistBtn = document.getElementById('wishlist');
+
+    const goodsWrapper = document.querySelector('.goods-wrapper');
+
+
+    const createCardGoods = (id, title, price, img) => {
+        const card = document.createElement('div');
+        card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
+
+        card.innerHTML = `
+            <div class="card">
+                <div class="card-img-wrapper">
+                    <img class="card-img-top" src="${img}" alt="">
+                    <button class="card-add-wishlist" data-goods-id="${id}"></button>
+                </div>
+                <div class="card-body justify-content-between">
+                    <a href="#" class="card-title">${title}</a>
+                    <div class="card-price">${price}</div>
+                    <div>
+                        <button class="card-add-cart" data-goods-id="${id}">Добавить в корзину</button>
+                    </div>
+                </div>
+            </div> `
+        return card;
+
+    }
+
+
+    goodsWrapper.append(createCardGoods(1, 'Дартс', '200', 'img/temp/Archer.jpg'));
+    goodsWrapper.append(createCardGoods(1, 'Дартс', '300', 'img/temp/Archer.jpg'));
+
+    
+
+
+
+
+
+
+
+
+});
